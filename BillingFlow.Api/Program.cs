fa0e815f560ee68ee8e6d0bc64a7b3eed62bf6a6
Daniel <daniel-zanelato@hotmail.com>
2026-04-23 14:31:59 -0300

    Adicionando migration automatica

--- BillingFlow.Api/Program.cs
+++ BillingFlow.Api/Program.cs
@@ -87,6 +87,12 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<BillingFlowDbContext>();
+    dbContext.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
